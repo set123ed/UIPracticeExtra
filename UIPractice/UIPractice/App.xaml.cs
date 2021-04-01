f57620195b69d2879e36738de4b06dff0ee277a8
set123ed <set123ed@gmail.com>
2021-04-01 00:07:36 -0400

    Adding Prism

---
+++ UIPractice/UIPractice/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UIPractice
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer platformInitializer = null) : base(platformInitializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+    }
+}
